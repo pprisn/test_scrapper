e677c16b37fb62a4023c8aae3d9f9858171ef9bd
ppri <pprisn@yandex.ru>
2020-03-15 23:16:03 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM golang:latest
 #FROM scratch
+# Add Maintainer Info
+LABEL maintainer="Sergey Popurey <pprisn@yandex.ru>"
 ARG LOG_DIR=/app/logs
 # Create Log Directory
 RUN mkdir -p ${LOG_DIR}
@@ -8,9 +10,16 @@ RUN mkdir -p ${LOG_DIR}
 ENV LOG_FILE_LOCATION=${LOG_DIR}/test_scrapper.log 
 # Set the Current Working Directory inside the container
 WORKDIR /app
+# Copy go mod and sum files
+COPY go.mod go.sum ./
+# Download all dependencies. Dependencies will be cached if the go.mod and go.sum files are not changed
+RUN go mod download
+# Copy the source from the current directory to the Working Directory inside the container
+COPY . .
+RUN make
 EXPOSE 8000
 ADD ca-certificates.crt /etc/ssl/certs/
-ADD test_scrapper /app
+#ADD test_scrapper /app
 ADD .env /app
 # Declare volumes to mount
 VOLUME [${LOG_DIR}]
--- test/get_user_url.sh
+++ test/get_user_url.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
-TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjUsImV4cCI6MTU4Mzk1NzMwMiwiaXNzIjoidGVzdCJ9.yYfOPoDmbuxtLrB5eftRx1MeBYREDrG_MHG5GrbrUik
-curl -d '{"name" : "gfacebook.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjUsImV4cCI6MTU4NDMwODE2OCwiaXNzIjoidGVzdCJ9.rwclejX5B9mScuPFco6xnbZx4Br74DBPNhOPBFH0sQE
+#curl -d '{"name" : "gfacebook.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
 #Response
 #{"data":{"ID":3,"CreatedAt":"0001-01-01T00:00:00Z","UpdatedAt":"2020-03-11T12:33:53+03:00","DeletedAt":null,"name":"gfacebook.com","timeout80":"1","timeout443":"2"},"message":"success","status":true}
+curl -d '{"name" : "ggoogle.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "youtube.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "facebook.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "baidu.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "wikipedia.org", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "qq.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "taobao.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "yahoo.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "tmall.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "amazon.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.co.in", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "twitter.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "sohu.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "jd.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "live.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "instagram.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "sina.com.cn", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "weibo.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.co.jp", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "reddit.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "vk.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "360.cn", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "login.tmall.com ", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "blogspot.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "yandex.ru", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.com.hk", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "netflix.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "linkedin.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "pornhub.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.com.br", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "twitch.tv", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "pages.tmall.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "csdn.net", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "yahoo.co.jp", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "mail.ru", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "aliexpress.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "alipay.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "office.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.fr", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.ru", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "google.co.uk", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
+curl -d '{"name" : "microsoftonline.com", "timeout80" : "", "timeout443" : ""}' -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}" -X POST http://localhost:8000/api/user/url
