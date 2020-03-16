d981fb0bfbf3ba8ba7bd16027559f60cbbd20020
ppri <pprisn@yandex.ru>
2020-03-16 06:29:32 +0300

    backup

--- DockerRun.sh
+++ DockerRun.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/logs/go-docker
-docker run --rm -d -p 8000:8000 -it --network="host" -v ~/logs/go-docker:/app/logs  test_scrapper
+docker run --rm -d -p 8000:8000 -it --cpus="1.5"  --network="host" -v ~/logs/go-docker:/app/logs  test_scrapper
